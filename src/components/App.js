c823b4938de11641389c946a539308ffef43db88
Gabriel Nogueira <gfnogueira@gmail.com>
2018-06-22 23:51:34 -0300

    connect App to store

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
+    console.log('Props', this.props);
     return <div>Hello World</div>;
   }
 }
 
-export default App;
+function mapStateToProps(calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+  return {
+    calendar: dayOrder.map(day => ({
+      day,
+      meals: Object.assign({}, calendar[day])
+    }))
+  };
+}
+
+export default connect(mapStateToProps)(App);
