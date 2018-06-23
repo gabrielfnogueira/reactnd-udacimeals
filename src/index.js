07ab2a7e27ccbe8bdc95582f782315ab2861835d
Gabriel Nogueira <gfnogueira@gmail.com>
2018-06-22 23:31:50 -0300

    add react-redux dependency and use its Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
 import './index.css';
@@ -8,7 +9,10 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-console.log(store);
-
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
